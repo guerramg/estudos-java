1c338a196fe44da1ebb0184784423820f55e2e77
guerramg <agenciaintuito@gmail.com>
2023-02-09 10:10:36 -0300

    java basico

---
+++ sintaxe-variaveis-e-fluxo/src/Resto3.java
@@ -0,0 +1,14 @@
+
+public class Resto3 {
+
+	public static void main(String[] args) {
+
+		for(int resto = 0; resto <=100; resto++) {
+			if(resto % 3 == 0) {
+				System.out.println(resto);
+			}
+		}
+
+	}
+
+}
